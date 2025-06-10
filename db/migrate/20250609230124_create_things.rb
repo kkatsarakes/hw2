37a0bb9af52220c53ff4861247eecd878e2d34c6
Kiki Katsarakes <kiki.katsarakes@kellogg.northwestern.edu>
2025-06-10 04:08:37 +0000

    second

--- db/migrate/20250609230124_create_things.rb
+++ db/migrate/20250609230124_create_things.rb
@@ -1,7 +1,10 @@
 class CreateThings < ActiveRecord::Migration[7.1]
   def change
     create_table :things do |t|
-
+      t.string "name"
+      t.string "city"
+      t.string "state"
+      t.string "url"
       t.timestamps
     end
   end
